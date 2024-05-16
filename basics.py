669a83219e637fb1e9f01a947e3e0c9ed4bb4747
ashok <ashokreddyothers@gmail.com>
2024-05-16 22:45:57 +0530

    add

---
+++ basics.py
@@ -0,0 +1,88 @@
+# a = "ashok"
+# b = "kumar"
+# c = a+b
+# print ((c.upper()))
+# print ((a.upper())) 
+# a = [1,2,31]
+# a.pop()
+# print (a)
+# a.append(23)
+# print (a)
+
+# a=[1,2,3]
+# a[0]=10
+# print(a)
+# a=[1,2,3]
+# a.clear()
+# print (a)
+
+# a=[1,2,3]
+# a.insert(1,10)
+# print (a)
+
+# a=[1,2,3]
+ 
+# print (a.pop())
+# print (a)
+ 
+# a=[1,2,3]
+ 
+# a.pop()
+# print (a)
+ 
+# a=[30,20,10]
+# a.sort()
+# print (a)
+# a=["ashok","kumar", "gorantla"]
+# a.sort()
+# print (a)
+# a=["ashok","ashok1", "gorantla", "GORANTLA"]
+# a.sort()
+# print (a)
+# a=[5,6,8]
+# del a[0]
+# print (a)
+
+# a="ashok kumar"
+# a.split()
+# print (a.split())
+# a= "ashok kumar reddy"
+# b=a.split()
+# print (b)
+# print (type(b))
+
+# a="local aws kumar"
+# # b=a.split('a')
+# print (a.split('a'))
+
+
+
+
+
+# a=[1,2,3]
+# a=[1,2,3,4,5,6,7,8]
+# a.sort()
+# print (a
+
+# a={'name':'ashok', 'firstname' : 'gorantla'}
+# print (a['name'])
+# print(type(a))
+# print(type(a['name']))
+
+# a={'marks':[1,2,3], 'name': 'ashok'}
+# b=a['marks'][0]=2
+# print (a)
+# print (b)
+# a['name']='kumar'  # name is substituted
+# print (a)
+
+# #mukti string  , for it we use 3 quotes
+# a= """ashok,
+# kumar,
+# reddy
+# """
+# print (a.split())
+
+a={'marks': '1,2,3', 'name': 'ashok'}
+a.popitem()
+print(a.popitem())
---
+++ interview.py
@@ -0,0 +1,5 @@
+# HOW YOU TAKE INPUT FROM USER?
+    by using input method as shown above
+    
+    
+############################################################################################
