cb8ca23ba1b0b63802ff0112a2a99b7ea5be6886
ashok <ashokreddyothers@gmail.com>
2024-10-10 15:22:35 +0530

    add

---
+++ 00-python-interview/00-python-interview-coding.py
@@ -0,0 +1,34 @@
+
+# ======================================================================
+# write a shell script for this scenario, take two varialbes and  in third varialbe i need sum of those  two varialvbles, but i want enter values dynamically, 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+
+print("The sum of", num1, "and", num2, "is:", sum)
+
+#==============================================================================
+
+#=============numbers from 1-100==============
+for num in range(1, 101):
+    print(num)
+    
+#===================== even numbers=================
+
+
+# ================odd numbers ============================
+
+
+# ======================================================================
+# write a shell script for this scenario, take two varialbes and  in third varialbe i need sum of those  two varialvbles, but i want enter values dynamically, 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+
+print("The sum of", num1, "and", num2, "is:", sum)
+
+#==============================================================================
+
+
