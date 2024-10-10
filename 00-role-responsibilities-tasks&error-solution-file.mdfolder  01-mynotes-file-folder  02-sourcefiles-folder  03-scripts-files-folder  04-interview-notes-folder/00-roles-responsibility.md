47c85fb9c7ed1ed7f1cbf8bfef618c3d24c39e10
ashok <ashokreddyothers@gmail.com>
2024-10-10 15:40:52 +0530

    add

---
+++ 00-role-responsibilities-tasks&error-solution-file.mdfolder  01-mynotes-file-folder  02-sourcefiles-folder  03-scripts-files-folder  04-interview-notes-folder/00-roles-responsibility.md
@@ -0,0 +1,6 @@
+---
+# ========================================1-tasks collected from other sources  as references =============
+- - backup and recovery 
+- to interact with end points and get the data and parsing that data
+- to syncranization of database
+- 
--- 04-python-interview/00-python-coding-Q-asked-in-interview.py
+++ 04-python-interview/00-python-coding-Q-asked-in-interview.py
@@ -1,4 +1,7 @@
 
+#=========================== 1-python tasks asked in interview  ======================
+
+
 # ======================================================================
 # write a shell script for this scenario, take two varialbes and  in third varialbe i need sum of those  two varialvbles, but i want enter values dynamically, 
 num1 = int(input("Enter the first number: "))
@@ -32,3 +35,8 @@ print("The sum of", num1, "and", num2, "is:", sum)
 #==============================================================================
 
 
+- i have given a json or csv file whihc has conssit of 5 people of data like name, age , salary . now i want to avg of all employess , write a code for it.
+- 
+
+
+-
